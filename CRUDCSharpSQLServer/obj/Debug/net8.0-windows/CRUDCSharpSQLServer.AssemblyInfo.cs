527d2fc649738e84e32fdc2a4bcb2d8375fd7c4c
NorthSideBoy <saidlucena519@gmail.com>
2024-12-19 15:25:48 -0400

    git ignore bin and obj

--- CRUDCSharpSQLServer/obj/Debug/net8.0-windows/CRUDCSharpSQLServer.AssemblyInfo.cs
+++ CRUDCSharpSQLServer/obj/Debug/net8.0-windows/CRUDCSharpSQLServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDCSharpSQLServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3f624d5f9757099b2d5be8c1cecd437d3930af")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDCSharpSQLServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDCSharpSQLServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
